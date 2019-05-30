725c71c6e08606a301d3289b10cf13bfa406da63
Paul G Webster <paul.g.webster@googlemail.com>
2019-05-30 20:53:01 +0100

    Working POC

--- Prototype1/Crassus/Program.cs
+++ Prototype1/Crassus/Program.cs
@@ -8,12 +8,15 @@ namespace Crassus
 {
     public class Program
     {
+        // Horrible globals that are going to cause god knows how many memcpy's
         static Dictionary<string, channel> Channels = new Dictionary<string, channel>();
+        static Dictionary<string, Dictionary<string,bool>> Subscriptions = new Dictionary<string, Dictionary<string, bool>>();
+        static Dictionary<string, WebSocket> Sockets = new Dictionary<string, WebSocket>();
 
         // Horrible way of making out switch work, thank god for POCs
         const string SUBSCRIBE = @"SUBSCRIBE";
         const string UNSUBSCRIBE = @"UNSUBSCRIBE";
-        const string STREAMDATA = @"STREAMDATA";
+        const string DATA = @"DATA";
 
         [Obsolete]
         public static void Main(string[] args)
@@ -25,7 +28,7 @@ namespace Crassus
             var WebsocketServer = new WebSocketServer("ws://127.0.0.1:8080");
             WebsocketServer.AddWebSocketService("/", () => new ChannelAction("/"));
             WebsocketServer.Start();
-
+            
             Console.ReadKey(true);
             WebsocketServer.Stop();
         }
@@ -35,8 +38,6 @@ namespace Crassus
             private string _name = string.Empty;
             private string _prefix = string.Empty;
             private string Channel = string.Empty;
-
-            private Dictionary<string, bool> SubscribedChannels = new Dictionary<string, bool>();
 
             public ChannelAction()
                 : this(null)
@@ -50,7 +51,14 @@ namespace Crassus
 
             protected override void OnClose(CloseEventArgs Packet)
             {
-                Sessions.Broadcast(string.Format("{0} got logged off...", _name));
+                Console.WriteLine("WebSocket closed: {0}", ID);
+                Subscriptions.Remove(ID);
+            }
+
+            protected override void OnOpen()
+            {
+                Console.WriteLine("WebSocket open: {0}", ID);
+                Subscriptions.Add(ID, new Dictionary<string, bool>());
             }
 
             protected override void OnMessage(MessageEventArgs Packet)
@@ -68,50 +76,85 @@ namespace Crassus
                     Console.WriteLine("An exception was caused when deserializing '{0}', the exception raised was: '{1}'", Packet.Data, Exception.Message);
                     return;
                 }
+                catch (Newtonsoft.Json.JsonReaderException Exception)
+                {
+                    Console.WriteLine("An exception was caused when deserializing '{0}', the exception raised was: '{1}'", Packet.Data, Exception.Message);
+                    return;
+                }
 
                 bool SkipBroadcast = false;
 
                 switch (DataPacket.action.ToUpper())
                 {
-                    case STREAMDATA:
+                    case DATA:
+                        Channel = DataPacket.args[0].ToUpper();
 
+                        if (DataPacket.args.Length == 0)
+                        {
+                            packet DataStreamError = new packet();
+                            DataStreamError.action = @"DATA";
+                            DataStreamError.args = new string[] { @"FAIL", @"No DATA" };
+                        }
                         break;
                     case SUBSCRIBE:
                         Channel = DataPacket.args[0].ToUpper();
 
-                        packet DataResponse = new packet();
-                        DataResponse.action = @"SUBSCRIBE";
+                        packet SubscribeResponse = new packet();
+                        SubscribeResponse.action = @"SUBSCRIBE";
 
-                        if (SubscribedChannels.ContainsKey(Channel))
+                        if (!Sockets.ContainsKey(ID))
                         {
-                            // Return an error? Or just ignore it
+                            Sockets.Add(ID, Context.WebSocket);
+                        }
 
-                            DataResponse.args = new string[] { @"FAIL", Channel, @"You are already subscribed" };
-                            Send(JsonConvert.SerializeObject(DataResponse));
+                        if (!Channels.ContainsKey(Channel))
+                        {
+                            SubscribeResponse.args = new string[] { @"FAIL", Channel, @"No such channel" };
+                            Send(JsonConvert.SerializeObject(SubscribeResponse));
 
-                            Console.WriteLine("Cannot subscribe client to channel, they are already in it: '{0}'", Channel);
+                            Console.WriteLine("Cannot subscribe client to channel, channel does not exist: '{0}'", Channel);
                         }
                         else
                         {
-                            DataResponse.args = new string[] { @"SUCCESS", Channel };
-                            Send(JsonConvert.SerializeObject(DataResponse));
+                            if (Subscriptions[ID].ContainsKey(Channel))
+                            {
+                                SubscribeResponse.args = new string[] { @"FAIL", Channel, @"You are already subscribed" };
+                                Send(JsonConvert.SerializeObject(SubscribeResponse));
 
-                            SubscribedChannels.Add(Channel, true);
+                                Console.WriteLine("Cannot subscribe client to channel, they are already in it: '{0}'", Channel);
+                            }
+                            else
+                            {
+                                SubscribeResponse.args = new string[] { @"SUCCESS", Channel };
+                                Send(JsonConvert.SerializeObject(SubscribeResponse));
 
-                            Console.WriteLine("Client subscribed to channel: '{0}'", Channel);
+                                Subscriptions[ID].Add(Channel, true);
+
+                                Console.WriteLine("Client subscribed to channel: '{0}'", Channel);
+                            }
                         }
                         SkipBroadcast = true;
                         break;
                     case UNSUBSCRIBE:
                         Channel = DataPacket.args[0].ToUpper();
-                        if (SubscribedChannels.ContainsKey(Channel))
+
+                        packet UnSubscribeResponse = new packet();
+                        UnSubscribeResponse.action = @"UNSUBSCRIBE";
+
+                        if (Subscriptions[ID].ContainsKey(Channel))
                         {
-                            SubscribedChannels.Remove(Channel);
+                            UnSubscribeResponse.args = new string[] { @"SUCCESS", Channel };
+                            Send(JsonConvert.SerializeObject(UnSubscribeResponse));
+
+                            Subscriptions[ID].Remove(Channel);
+
                             Console.WriteLine("Client unsubscribed from channel: '{0}'", Channel);
                         }
                         else
                         {
-                            // Return an error? Or just ignore it
+                            UnSubscribeResponse.args = new string[] { @"FAIL", Channel, @"You are already subscribed" };
+                            Send(JsonConvert.SerializeObject(UnSubscribeResponse));
+
                             Console.WriteLine("Cannot unsubscribe client from channel, they are not in it: '{0}'", Channel);
                         }
                         SkipBroadcast = true;
@@ -123,14 +166,32 @@ namespace Crassus
                 }
 
                 if (!SkipBroadcast) {
-                    
-                    //Sessions.Broadcast(string.Format(Packet.Data));
+                    //packet BroadcastMessage = new packet();
+
+                    //BroadcastMessage.action = @"DATA";
+                    //BroadcastMessage.args = new string[] { Channel, DataPacket.args[1] };
+
+                    //string BroadcastMessageAsString = JsonConvert.SerializeObject(BroadcastMessage);
+
+                    /* 
+                     * To get this to work we need to rework the Broadcast call so that it is possible to check
+                     * if each client has acctually subscribed to whatever 'Channel' is being sent to
+                     */
+
+                    foreach (string SesssionID in Sessions.IDs) {
+                        Console.WriteLine("Sender session({0}), target session({1})",ID,SesssionID);
+                        if (ID.Equals(SesssionID))
+                        {
+                            continue;
+                        }
+                        if (Subscriptions[SesssionID].ContainsKey(Channel))
+                        {
+                            Console.WriteLine("Send datapacket to {0}", SesssionID);
+                            Sockets[SesssionID].Send(Packet.Data);
+                        }
+                    }
                 }
             }
-
-            //protected override void OnOpen()
-            //{
-            //}
         }
 
         public class packet
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # crassus-csharp-mvp
 The csharp Framework MVP for crassus
+
+# Valid channels
+Valid channels are: 
+- CHAN1
+- CHAN2 
+- CHAN3
+
+# Example inputs (JSON)
+- {"action":"SUBSCRIBE","args":["CHAN1"]}
+- {"action":"DATA","args":["CHAN1","Test data"]}
+- {"action":"UNSUBSCRIBE","args":["CHAN1"]}
